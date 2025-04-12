2a9efa57fd62b912ee8de6d112798d7d4e3719ce
4motionEnjoyer <leevi.sulonen@protonmail.com>
2025-04-12 21:43:21 +0300

    Updated paths

--- run.sh
+++ run.sh
@@ -4,28 +4,12 @@ HOMEDIR=$(eval echo ~$USER)
 WORKDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 VENV_PATH="$HOMEDIR/marauder-uart-gui-python3venv"
 DESKTOP_FILE="$HOMEDIR/Desktop/mUg.desktop"
-DESKTOP_FILE="$HOMEDIR/Työpöytä/mUg.desktop" #comment this line if you don't have the finnish legends -package.
+#DESKTOP_FILE="$HOMEDIR/Työpöytä/mUg.desktop" #comment this line if you don't have the finnish legends -package.
 
 
+VENV_PATH="$HOMEDIR/marauder-uart-gui-python3venv"
 
-# Get the user's home directory dynamically
-USER_HOME=$(eval echo ~$USER)
-
-# Define the path to the virtual environment
-VENV_PATH="$USER_HOME/marauder-uart-gui-python3venv"
-REQUIREMENTS_FILE="$WORKDIR/dependencies.txt"
-
-# Activate the virtual environment
 source "$VENV_PATH/bin/activate"
-
-# Install libraries from requirements.txt if it exists
-if [ -f "$REQUIREMENTS_FILE" ]; then
-    echo "Installing dependencies from $REQUIREMENTS_FILE..."
-    pip3 install -r "$REQUIREMENTS_FILE"
-else
-    echo "$REQUIREMENTS_FILE not found. Skipping installation."
-fi
-
 python $WORKDIR/main.pyw
 deactivate
 
